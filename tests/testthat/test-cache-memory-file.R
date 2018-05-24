d023c6b600f8560dd3c6910db09af70ab4e399c1
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-05-24 15:17:31 +0300

    Removed redundant tests from test-cache-memory-file

--- tests/testthat/test-cache-memory-file.R
+++ tests/testthat/test-cache-memory-file.R
@@ -20,46 +20,6 @@ test_that("add_group() works", {
 })
 
 
-test_that("add_group() stops with NULL", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    expect_error(cache_fmem_test$add_group(NULL))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("add_group() stops with NA", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    expect_error(cache_fmem_test$add_group(NA))
-    cache_fmem_test$terminate()
-})
-
-test_that("add_group() stops with empty string", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    expect_error(cache_fmem_test$add_group(character()))
-    cache_fmem_test$terminate()
-})
-
-
-test_that("add_group() works with vector input", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    expect_error(
-        cache_fmem_test$add_group(c("a_group", "b_group")))
-    expect_error(
-        cache_fmem_test$add_group(c("a_group", NA_character_)))
-    expect_error(
-        cache_fmem_test$add_group(c(NA_character_, "b_group")))
-    expect_error(
-        cache_fmem_test$add_group(c(NA_character_, NA_character_)))
-    
-    cache_fmem_test$terminate()
-})
-
-
 # has_group tests ----------------------------------------------------
 test_that("has_group() works", {
     
@@ -72,59 +32,6 @@ test_that("has_group() works", {
     cache_fmem_test$add_group(fn_group)
     expect_true(cache_fmem_test$has_group(fn_group))
     expect_true(fs::dir_exists(cache_group_dir))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_group() stops with NULL", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    cache_fmem_test$add_group(fn_group)
-    
-    expect_error(cache_fmem_test$has_group(NULL))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_group() stops with NA", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    
-    expect_error(cache_fmem_test$has_group(NA))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_group() stops with empty string", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    
-    expect_error(cache_fmem_test$has_group(character()))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_group() stops with vector", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    
-    expect_error(
-        cache_fmem_test$has_group(c(fn_group, "b_group")))
-    expect_error(
-        cache_fmem_test$has_group(c(NA, NA)))
-    expect_error(
-        cache_fmem_test$has_group(c(NA, "b_group")))
-    expect_error(
-        cache_fmem_test$has_group(c("a_group", NA)))
     
     cache_fmem_test$terminate()
 })
@@ -166,79 +73,6 @@ test_that("delete_group() works with non existent group", {
 })
 
 
-test_that("delete_group() stops with NULL", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    expect_error(
-        cache_fmem_test$delete_group(NULL))
-    expect_true(fs::dir_exists(cache_group_dir))
-    expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("delete_group() stops with NA", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    expect_error(
-        cache_fmem_test$delete_group(NA))
-    expect_true(fs::dir_exists(cache_group_dir))
-    expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("delete_group() stops with empty string", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    expect_error(
-        cache_fmem_test$delete_group(character()))
-    expect_true(fs::dir_exists(cache_group_dir))
-    expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("delete_group() stops with vectors", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_group(fn_group)
-    cache_fmem_test$add_group("a_group")
-    cache_group_dir1 <- fs::path(cache_dir, fn_group)
-    cache_group_dir2 <- fs::path(cache_dir, "a_group")
-    
-    expect_error(
-        cache_fmem_test$delete_group(c(fn_group,"a_group")))
-    expect_true(fs::dir_exists(cache_group_dir1))
-    expect_true(fs::dir_exists(cache_group_dir2))
-    expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    expect_true(base::exists("a_group",
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
 # forget_group tests ----------------------------------------------------
 test_that("forget_group() works", {
     cache_fmem_test <- cache_memory_file(cache_dir)
@@ -255,72 +89,6 @@ test_that("forget_group() works", {
         fn_group,
         envir = cache_fmem_test$cache_env,
         inherits = FALSE)), NULL)
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("forget_group() stops with NULL", {
-    
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    expect_error(
-        cache_fmem_test$forget_group(NULL))
-    expect_true(length(fs::dir_ls(cache_group_dir)) != 0)
-    expect_equal(names(base::get0(
-        fn_group,
-        envir = cache_fmem_test$cache_env,
-        inherits = FALSE)), "key")
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("forget_group() stops with NA", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-    
-    expect_error(
-        cache_fmem_test$forget_group(NA))
-    expect_error(
-        cache_fmem_test$forget_group(c(NA, NA)))
-    expect_true(length(fs::dir_ls(cache_group_dir)) != 0)
-    expect_equal(names(base::get0(
-        fn_group,
-        envir = cache_fmem_test$cache_env,
-        inherits = FALSE)), "key")
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("forget_group() stops with vector", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data("a_group", "key1", "value1")
-    
-    expect_error(
-        cache_fmem_test$forget_group(c()))
-    expect_error(
-        cache_fmem_test$forget_group(c(fn_group, "a_group")))
-    expect_error(
-        cache_fmem_test$forget_group(c(NA, "a_group")))
-    expect_error(
-        cache_fmem_test$forget_group(c(fn_group, NA)))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("forget_group() stops with empty string", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    
-    expect_error(
-        cache_fmem_test$forget_group(character()))
     
     cache_fmem_test$terminate()
 })
@@ -366,58 +134,6 @@ test_that("list_keys() works with non-existent group", {
 })
 
 
-test_that("list_keys() stops with NULL", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-
-    expect_error(
-        cache_fmem_test$list_keys(NULL))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("list_keys() stops with NA", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-
-    expect_error(
-        cache_fmem_test$list_keys(NA))
-    expect_error(
-        cache_fmem_test$list_keys(c(NA, NA)))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("list_keys() stops with empty string", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-
-    expect_error(
-        cache_fmem_test$list_keys(character()))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("list_keys stops with vectors", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data("a_group", "key1", "value1")
-
-    expect_error(
-        cache_fmem_test$list_keys(c(fn_group, "a_group")))
-    expect_error(
-        cache_fmem_test$list_keys(c(NA, "a_group")))
-    expect_error(
-        cache_fmem_test$list_keys(c(fn_group, NA)))
-
-    cache_fmem_test$terminate()
-})
-
-
 # has_key tests ----------------------------------------------------
 test_that("has_key() works", {
 
@@ -437,66 +153,6 @@ test_that("has_key() works", {
     expect_true(base::exists(fn_group,
                              envir = cache_fmem_test$cache_env, 
                              inherits = FALSE))
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_key() stops with key vector", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(
-        cache_fmem_test$has_key(fn_group, c("key", "key1")))
-    expect_error(
-        cache_fmem_test$has_key(fn_group, c("key", "key2")))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_key() stops with key NA", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(
-        cache_fmem_test$has_key(fn_group, NA))
-    expect_error(
-        cache_fmem_test$has_key(fn_group, c(NA, NA)))
-    expect_error(
-        cache_fmem_test$has_key(fn_group, c(NA, "key1")))
-    expect_error(
-        cache_fmem_test$has_key(fn_group, c("key", NA)))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_key() stops with key NULL", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(
-        cache_fmem_test$has_key(fn_group, NULL))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("has_key() stops with key empty string", {
-
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(
-        cache_fmem_test$has_key(fn_group, character()))
-
     cache_fmem_test$terminate()
 })
 
@@ -526,53 +182,6 @@ test_that("get_data() stops with non-existent group", {
 })
 
 
-test_that("get_data() stops with NULL key", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(cache_fmem_test$get_data(fn_group, NULL))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("get_data() stops with NA key", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(cache_fmem_test$get_data(fn_group, NA))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("get_data() stops with empty string key", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(cache_fmem_test$get_data(fn_group, character()))
-
-    cache_fmem_test$terminate()
-})
-
-
-test_that("get_data() stops with vector of keys", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_fmem_test$add_data(fn_group, "key", "value")
-    cache_fmem_test$add_data(fn_group, "key1", "value1")
-
-    expect_error(cache_fmem_test$get_data(fn_group, c("key", "key1")))
-    expect_error(cache_fmem_test$get_data(fn_group, c(NA, "key1")))
-    expect_error(cache_fmem_test$get_data(fn_group, c("key", NA)))
-    expect_error(cache_fmem_test$get_data(fn_group, c(NA, NA)))
-
-    cache_fmem_test$terminate()
-})
-
-
 # add_data tests ----------------------------------------------------------
 test_that("add_data() works", {
     cache_fmem_test <- cache_memory_file(cache_dir)
@@ -591,23 +200,6 @@ test_that("add_data() works", {
     cache_fmem_test$terminate()
 })
 
-
-test_that("add_data() stops with NULL args", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-
-    expect_error(cache_fmem_test$add_data(NULL, "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, NULL, "value"))
-    expect_false(fs::dir_exists(cache_group_dir))
-    expect_false(fs::file_exists(fs::path(cache_group_dir, "key")))
-    expect_false(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
 test_that("add_data() works with NULL value", {
     cache_fmem_test <- cache_memory_file(cache_dir)
     cache_group_dir <- fs::path(cache_dir, fn_group)
@@ -616,22 +208,6 @@ test_that("add_data() works with NULL value", {
     expect_true(fs::dir_exists(cache_group_dir))
     expect_true(fs::file_exists(fs::path(cache_group_dir, "key")))
     expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("add_data() stops with NA", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-
-    expect_error(cache_fmem_test$add_data(NA, "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, NA, "value"))
-    expect_false(fs::dir_exists(cache_group_dir))
-    expect_false(fs::file_exists(fs::path(cache_group_dir, "key")))
-    expect_false(base::exists(fn_group,
                              envir = cache_fmem_test$cache_env, 
                              inherits = FALSE))
     
@@ -655,29 +231,6 @@ test_that("add_data() works with NA values", {
 })
 
 
-test_that("add_data() stops with vectors", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-
-    expect_error(cache_fmem_test$add_data(c("a", "b"), "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, c("a", "b"), "value"))
-
-    expect_error(cache_fmem_test$add_data(c(NA, "b"), "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, c(NA, "b"), "value"))
-
-    expect_error(cache_fmem_test$add_data(c(NA, NA), "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, c(NA, NA), "value"))
-
-    expect_false(fs::dir_exists(cache_group_dir))
-    expect_false(fs::file_exists(fs::path(cache_group_dir, "key")))
-    expect_false(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
 test_that("add_data() works with vector values", {
     cache_fmem_test <- cache_memory_file(cache_dir)
     cache_group_dir <- fs::path(cache_dir, fn_group)
@@ -687,22 +240,6 @@ test_that("add_data() works with vector values", {
     expect_true(fs::dir_exists(cache_group_dir))
     expect_true(fs::file_exists(fs::path(cache_group_dir, "key")))
     expect_true(base::exists(fn_group,
-                             envir = cache_fmem_test$cache_env, 
-                             inherits = FALSE))
-    
-    cache_fmem_test$terminate()
-})
-
-
-test_that("add_data() stops with empty strings", {
-    cache_fmem_test <- cache_memory_file(cache_dir)
-    cache_group_dir <- fs::path(cache_dir, fn_group)
-
-    expect_error(cache_fmem_test$add_data(character(), "key", "value"))
-    expect_error(cache_fmem_test$add_data(fn_group, character(), "value"))
-    expect_false(fs::dir_exists(cache_group_dir))
-    expect_false(fs::file_exists(fs::path(cache_group_dir, "key")))
-    expect_false(base::exists(fn_group,
                              envir = cache_fmem_test$cache_env, 
                              inherits = FALSE))
     
