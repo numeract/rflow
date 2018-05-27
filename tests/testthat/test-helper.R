d66ae8b8ccea5995e717f12c3b909f62051a3331
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-05-27 13:04:53 +0300

    Added more detailed is_key and require_keys tests

--- tests/testthat/test-helper.R
+++ tests/testthat/test-helper.R
@@ -7,29 +7,72 @@ test_that("is_key() works", {
 })
 
 
-test_that("is_key() stops with non valid input", {
+test_that("is_key() stops with NA and NULL inputs", {
     
+    expect_false(is_key(NULL))
     expect_false(is_key(NA))
-    expect_false(is_key(character()))
+})
+
+
+test_that("is_key() stops with non character input", {
+    
+    expect_false(is_key(TRUE))
+    expect_false(is_key(list()))
+    expect_false(is_key(list(a = "a")))
+    expect_false(is_key(data.frame()))
+})
+
+
+test_that("is_key() stops with vectors", {
+    
+    expect_false(is_key(c()))
     expect_false(is_key(c("a_key", "b_key")))
     expect_false(is_key(c(NA, "b_key")))
-    expect_false(is_key(NULL))
-    expect_false(is_key(2))
+    expect_false(is_key(c("d", "b_key", NULL)))
+    expect_false(is_key(c("d", NULL, NULL, "a")))
+    # Shouldn't this be false?
+   # expect_false(is_key(c(NULL, "a")))
 })
+
 
 test_that("require_keys() works", {
     
-    expect_silent(require_keys("a_key", "key"))
+    expect_silent(require_keys())
+    expect_silent(require_keys("key1"))
+    expect_silent(require_keys("a_key", "b_key"))
+    expect_silent(require_keys("a_key", "b_key", "c_key"))
     
+ 
 })
 
 
-test_that("require_keys() stops with invalid input", {
+test_that("require_keys() stops with NA and NULL inputs", {
     
+    expect_error(require_keys(NULL))
+    expect_error(require_keys(NULL, "key"))
+    expect_error(require_keys("key", NULL))
+    expect_error(require_keys(NA))
     expect_error(require_keys(NA, "key"))
     expect_error(require_keys(NA, NA))
-    expect_error(require_keys(character()))
+   
+})
+
+test_that("require_keys() stops with non character input", {
+    
+    expect_error(require_keys(1, 2, "ana"))
+    expect_error(require_keys(TRUE))
+    expect_error(require_keys(list()))
+    expect_error(require_keys(data.frame()))
+})
+
+
+test_that("require_keys() stops with vectors", {
+    
+    expect_error(require_keys(c()))
     expect_error(require_keys(c("a_key", "b_key"), "a_key"))
     expect_error(require_keys(c(NA, "b_key")))
-    expect_error(require_keys(NULL, "b_key"))
+    expect_error(require_keys(c("d", "b_key", NULL)))
+    expect_error(require_keys(c("d", NULL, NULL, "a")))
+    # Shouldn't this throw an error?
+  #  expect_error(require_keys(c(NULL, "a")))
 })
