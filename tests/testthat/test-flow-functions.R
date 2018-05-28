7dec38724e326675fb71103798ad80c67396321f
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-05-28 14:29:05 +0300

    Added tests for flow functions

--- tests/testthat/test-flow-functions.R
+++ tests/testthat/test-flow-functions.R
@@ -81,3 +81,77 @@ test_that("flow_fn() works with non valid id", {
     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = Inf))
     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = -Inf))
 })
+
+
+# test flow 
+# test_that("flow() works", {
+#     test_flow_fn <- flow(test_fn(x = 1, y = 2))
+#     #collected_result <- test_flow_fn %>% collect()
+#     expect_true(TRUE)
+# })
+
+
+test_that("flow() stops with primitives", {
+    expect_error(test_flow <- flow(sum(2, 3)))
+})
+
+
+test_that("flow() stops with anonymus functions", {
+    expect_error(test_flow <- flow(function(x) {x}))
+})
+
+
+test_that("flow() stops with non function argument", {
+    expect_error(flow(1))
+    expect_error(flow("a_character"))
+    expect_error(flow(TRUE))
+    expect_error(flow(c(test_fn, test_fn2)))
+    expect_error(flow(NA))
+    expect_error(flow(NULL))
+    expect_error(flow(list()))
+})
+
+
+# test_that("flow() works with character fn_id", {
+#     test_flow <-
+#         flow_fn(test_fn(1, 2), fn_id = "id1")
+# 
+#     collected_result <- test_flow %>% collect()
+#     expect_equal(collected_result, 5)
+# })
+
+
+# test_that("flow_fn() works with integer fn_id", {
+#     test_flow_fn <-
+#         flow_fn(test_fn, fn_id = 1)
+#     rflow_test <- test_flow_fn(2, 3)
+#     collected_result <- rflow_test %>% collect()
+#     expect_equal(collected_result, 5)
+#     forget(rflow_test)
+# })
+# 
+# 
+# test_that("flow_fn() works with integer fn_id", {
+#     test_flow_fn <-
+#         flow_fn(test_fn, fn_id = 1)
+#     rflow_test <- test_flow_fn(2, 3)
+#     collected_result <- rflow_test %>% collect()
+#     expect_equal(collected_result, 5)
+#     forget(rflow_test)
+# })
+# 
+# 
+# test_that("flow_fn() works with non valid id", {
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = NA))
+#     
+#     # Shouldn't return an error?
+#     # expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = NA_character_))
+#     # expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = NA_integer_))
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = character()))
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = c("a", "b")))
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = c(1, 2)))
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = list()))
+#     
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = Inf))
+#     expect_error(test_flow_fn <- flow_fn(test_fn, fn_id = -Inf))
+# })
