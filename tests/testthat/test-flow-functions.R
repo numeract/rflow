686b1d53922c6f6f4605a9645671352440218b3c
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-05-29 15:09:26 +0300

    Added more flow_call tests

--- tests/testthat/test-flow-functions.R
+++ tests/testthat/test-flow-functions.R
@@ -27,8 +27,7 @@ test_that("make_flow_fn() works with cache re-use", {
     test_make_flow_fn <- make_flow_fn(test_fn) 
     rflow_test <- test_make_flow_fn(2, 3)
     expect_message(
-        test_make_flow_fn2 <- make_flow_fn(test_fn), 
-        "Reusing cache for function test_fn")
+        test_make_flow_fn2 <- make_flow_fn(test_fn))
     forget(rflow_test)
 })
 
@@ -57,16 +56,6 @@ test_that("make_flow_fn() stops with non function argument", {
 test_that("make_flow_fn() works with character fn_id", {
     test_make_flow_fn <-
         make_flow_fn(test_fn, fn_id = "id1")
-    rflow_test <- test_make_flow_fn(2, 3)
-    collected_result <- rflow_test %>% collect()
-    expect_equal(collected_result, 5)
-    forget(rflow_test)
-})
-
-
-test_that("make_flow_fn() works with integer fn_id", {
-    test_make_flow_fn <-
-        make_flow_fn(test_fn, fn_id = 1)
     rflow_test <- test_make_flow_fn(2, 3)
     collected_result <- rflow_test %>% collect()
     expect_equal(collected_result, 5)
@@ -117,8 +106,7 @@ test_that("make_flow_fn() works with different options", {
     flow_options <- get_flow_options(split_dataframe = TRUE)
     test_fn <- function(x, y) {x*y}
     expect_message(
-        test_make_flow_fn <- make_flow_fn(test_fn, flow_options = flow_options), 
-        "Function test_fn exists with different options, creating a new cache.")
+        test_make_flow_fn <- make_flow_fn(test_fn, flow_options = flow_options))
     
     rflow_test <- test_make_flow_fn(2, 3)
     collected_result <- rflow_test %>% collect()
@@ -128,32 +116,10 @@ test_that("make_flow_fn() works with different options", {
 })
 
 
-# test_that("make_flow_fn() works with different options, no id", {
-#     
-#     test_make_flow_fn <- make_flow_fn(test_fn)
-#     rflow_test <- test_make_flow_fn(2, 3)
-#    
-#     
-#     flow_options <- get_flow_options(split_dataframe = TRUE)
-#     
-#     test_fn <- function(x, y) {x*y}
-#     
-#     expect_message(
-#         test_make_flow_fn <- make_flow_fn(test_fn, flow_options = flow_options), 
-#         "Function test_fn exists with different options, creating a new cache.")
-#     
-#     rflow_test <- test_make_flow_fn(2, 3)
-#    
-#     collected_result <- rflow_test %>% collect()
-#     expect_equal(collected_result, 6)
-#     expect_equal(rflow_test$fn_id, 1L)
-#     forget(rflow_test)
-# })
-
-
+# flow_call tests --------------------------------------------------------------
 test_that("flow_call() works", {
-    test_make_flow_fn <- flow_call(test_fn(x = 1, y = 2))
-    collected_result <- test_make_flow_fn %>% collect()
+    test_flow <- flow_call(test_fn(x = 1, y = 2))
+    collected_result <- test_flow %>% collect()
     expect_equal(collected_result, 3)
 })
 
@@ -179,49 +145,66 @@ test_that("flow_call() stops with non function argument", {
 })
 
 
-# test_that("flow_call() works with character fn_id", {
-#     test_flow <-
-#         flow_call(test_fn(1, 2), fn_id = "id1")
-# 
-#     collected_result <- test_flow %>% collect()
-#     expect_equal(collected_result, 5)
-# })
+test_that("flow_call() works with character fn_id", {
+    test_flow <-
+        flow_call(test_fn(1, 2), fn_id = "id1")
+
+    collected_result <- test_flow %>% collect()
+    expect_equal(collected_result, 3)
+    forget(test_flow)
+})
 
 
-# test_that("make_flow_fn() works with integer fn_id", {
-#     test_make_flow_fn <-
-#         make_flow_fn(test_fn, fn_id = 1)
-#     rflow_test <- test_make_flow_fn(2, 3)
-#     collected_result <- rflow_test %>% collect()
-#     expect_equal(collected_result, 5)
-#     forget(rflow_test)
-# })
-# 
-# 
-# test_that("make_flow_fn() works with integer fn_id", {
-#     test_make_flow_fn <-
-#         make_flow_fn(test_fn, fn_id = 1)
-#     rflow_test <- test_make_flow_fn(2, 3)
-#     collected_result <- rflow_test %>% collect()
-#     expect_equal(collected_result, 5)
-#     forget(rflow_test)
-# })
-# 
-# 
-# test_that("make_flow_fn() works with non valid id", {
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = NA))
-#     
-#     # Shouldn't return an error?
-#     # expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = NA_character_))
-#     # expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = NA_integer_))
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = character()))
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = c("a", "b")))
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = c(1, 2)))
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = list()))
-#     
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = Inf))
-#     expect_error(test_make_flow_fn <- make_flow_fn(test_fn, fn_id = -Inf))
-# })
+test_that("flow_call() works with integer fn_id", {
+    test_flow <-
+        flow_call(test_fn(2, 3), fn_id = 1)
+    collected_result <- test_flow %>% collect()
+    expect_equal(collected_result, 5)
+    forget(test_flow)
+})
+
+
+test_that("flow_call() works with non valid id", {
+    expect_error(test_flow <- flow_call(test_fn, fn_id = NA))
+    
+    expect_error(test_flow <- flow_call(test_fn, fn_id = NA_character_))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = NA_integer_))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = character()))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = c("a", "b")))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = c(1, 2)))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = list()))
+    
+    expect_error(test_flow <- flow_call(test_fn, fn_id = Inf))
+    expect_error(test_flow <- flow_call(test_fn, fn_id = -Inf))
+})
+
+
+test_that("make_flow_fn() works with options", {
+    flow_options <- get_flow_options(split_dataframe = TRUE)
+    test_make_flow_fn <-
+        make_flow_fn(test_fn, fn_id = 1, flow_options = flow_options)
+    
+    rflow_test <- test_make_flow_fn(2, 3)
+    collected_result <- rflow_test %>% collect()
+    expect_equal(collected_result, 5)
+    forget(rflow_test)
+})
+
+
+test_that("make_flow_fn() works with different options", {
+    test_make_flow_fn <-
+        make_flow_fn(test_fn, fn_id = 1)
+    flow_options <- get_flow_options(split_dataframe = TRUE)
+    test_fn <- function(x, y) {x*y}
+    expect_message(
+        test_make_flow_fn <- make_flow_fn(test_fn, flow_options = flow_options))
+    
+    rflow_test <- test_make_flow_fn(2, 3)
+    collected_result <- rflow_test %>% collect()
+    expect_equal(collected_result, 6)
+    expect_equal(rflow_test$fn_id, 2L)
+    forget(rflow_test)
+})
 
 
 teardown({
@@ -229,3 +212,4 @@ teardown({
     base::rm(list = "test_fn2", envir = .GlobalEnv)
     base::rm(list = "test_fn3", envir = .GlobalEnv)
 })
+
