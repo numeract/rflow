991acff29a9ce48aca59c5fe242e97d5f6ca3eb0
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-06-08 14:56:16 +0300

    Added tests for different types of functions

--- tests/testthat/test-flow-dfg.R
+++ tests/testthat/test-flow-dfg.R
@@ -19,9 +19,47 @@ setup({
         df
     }
     
+    df_fn2 <- function(df, i = NULL) {
+        if (is.null(i)) {
+            dfi <- df
+        } else {
+            dfi <- df[i, , drop = FALSE]
+        }
+        dfi$rm <- rowMeans(dfi[1:10])
+        dfi
+    }
+    
+    df_fn3 <- function(df) {
+        df$Sepal.Length <- "Large"
+        df
+    }
+    
+    df_fn4 <- function(df) {
+        colnames(df)[2] <- "new_name"
+        df
+    }
+    
+    df_fn5 <- function(df) {
+        df2 <- dplyr::data_frame(
+            Sepal.Length = 1,
+            Sepal.Width = 2,
+            Petal.Length = 3,
+            Petal.Width = 4,
+            Species = "setosa"
+        )
+        
+        df <- df %>% dplyr::bind_rows(df2)
+        df
+    }
+    
+    
     identity_df <- function(df) {df}
     assign("df", df, envir = .GlobalEnv)
     assign("df_fn", df_fn, envir = .GlobalEnv)
+    assign("df_fn2", df_fn2, envir = .GlobalEnv)
+    assign("df_fn3", df_fn3, envir = .GlobalEnv)
+    assign("df_fn4", df_fn4, envir = .GlobalEnv)
+    assign("df_fn5", df_fn5, envir = .GlobalEnv)
     assign("identity_df", identity_df, envir = .GlobalEnv)
 })
 
@@ -111,9 +149,62 @@ test_that("flow_dfg stops with non valid group_by", {
 })
 
 
+test_that("flow_dfg stops with function that adds a column", {
+    dfg_test <- head(df, n = 35) %>%
+        dplyr::group_by(Species)
+    
+    dfg1 <- flow_dfg(dfg_test, fn = df_fn2)
+    
+    expect_error(dfg1 %>% collect())
+    
+    dfg1$eddy$reset()
+})
+
+
+# Shouldn't this throw an error?
+test_that("flow_dfg stops with function that modifies column type", {
+    dfg_test <- head(df, n = 35) %>%
+        dplyr::group_by(Species)
+    
+    dfg1 <- flow_dfg(dfg_test, fn = df_fn3)
+    
+    expect_error(dfg1 %>% collect())
+    
+    dfg1$eddy$reset()
+})
+
+
+# Shouldn't this throw an error?
+test_that("flow_dfg stops with function that changes column name", {
+    dfg_test <- head(df, n = 35) %>%
+        dplyr::group_by(Species)
+    
+    dfg1 <- flow_dfg(dfg_test, fn = df_fn4)
+    
+    expect_error(dfg1 %>% collect())
+    
+    dfg1$eddy$reset()
+})
+
+
+test_that("flow_dfg stops with function that adds new row", {
+    dfg_test <- head(df, n = 35) %>%
+        dplyr::group_by(Species)
+    
+    dfg1 <- flow_dfg(dfg_test, fn = df_fn5)
+    
+    expect_error(dfg1 %>% collect())
+    
+    dfg1$eddy$reset()
+})
+
 
 teardown({
     base::rm(list = "df", envir = .GlobalEnv)
     base::rm(list = "df_fn", envir = .GlobalEnv)
+    base::rm(list = "df_fn2", envir = .GlobalEnv)
+    base::rm(list = "df_fn3", envir = .GlobalEnv)
+    base::rm(list = "df_fn4", envir = .GlobalEnv)
+    base::rm(list = "df_fn5", envir = .GlobalEnv)
     base::rm(list = "identity_df", envir = .GlobalEnv)
 })
