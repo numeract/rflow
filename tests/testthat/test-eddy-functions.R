f28dbeecf5643db12488367f2b14a83b545b5bb2
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-07-19 13:45:45 +0300

    Added eddy tests

--- tests/testthat/test-eddy-functions.R
+++ tests/testthat/test-eddy-functions.R
@@ -7,6 +7,21 @@ if (digest::digest(Sys.info()[-c(2, 3)]) %in% c(
     skip("cache-memory-file functions")
 }
 
+
+test_that("new_eddy() works", {
+    eddy <- new_eddy("new_eddy_test")
+    expect_equal(get_eddy("new_eddy_test"), eddy)
+    delete_eddy("new_eddy_test") 
+})
+
+
+test_that("set_current_eddy() works", {
+    eddy <- new_eddy("eddy_test")
+    set_current_eddy("eddy_test")
+    expect_equal(get_current_eddy(), eddy)
+    delete_eddy("eddy_test") 
+})
+
 test_that("parse_flow_options() works", {
     eval_arg_fn <- function(x) {list(x)}
     eddy <- new_eddy(eddy_name = "test_eddy")
